7ee4ca361286b694ac8a757620b2f00678334cea
dennis273 <dennisc695@gmail.com>
2018-10-24 22:22:09 +0800

    Implement arguments in login.go

--- cmd/login.go
+++ cmd/login.go
@@ -15,36 +15,32 @@
 package cmd
 
 import (
-	"fmt"
-
-	"github.com/spf13/cobra"
+  "fmt"
+  "github.com/spf13/cobra"
 )
+
+var loginArgs struct {
+  username string
+  password string
+}
 
 // loginCmd represents the login command
 var loginCmd = &cobra.Command{
-	Use:   "login",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("login called")
-	},
+  Use:   "login",
+  Short: "Login by username and password.",
+  Long: `Login by username and password.`,
+  Run: func(cmd *cobra.Command, args []string) {
+    if len(loginArgs.username) == 0 && len(loginArgs.password) == 0 {
+      fmt.Println("Invalid arguments")
+      return
+    }
+    fmt.Printf("%s %s\n", loginArgs.username, loginArgs.password)
+  },
 }
 
 func init() {
-	rootCmd.AddCommand(loginCmd)
+  rootCmd.AddCommand(loginCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// loginCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// loginCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+  loginCmd.PersistentFlags().StringVarP(&(loginArgs.username), "username", "u", "", "Username")
+  loginCmd.PersistentFlags().StringVarP(&(loginArgs.password), "password", "p", "", "Password")
 }
