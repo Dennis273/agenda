44fd3ae798d38453d287c4211fde06993d4d8011
dennis273 <dennisc695@gmail.com>
2018-10-26 12:34:49 +0800

    Update deleteUser.go, logout.go, queryUser.go

--- cmd/deleteUser.go
+++ cmd/deleteUser.go
@@ -1,4 +1,4 @@
-// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
+// Copyright © 2018 Dennis273 <dennisc695@gmail.com>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -23,13 +23,9 @@ import (
 // deleteUserCmd represents the deleteUser command
 var deleteUserCmd = &cobra.Command{
 	Use:   "deleteUser",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Delete current User",
+	Long: `Delete the user that is currently logged in.
+	This operation would automatically remove you from every meetings that you participated in, and cancel meetings you holded.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("deleteUser called")
 	},
@@ -37,14 +33,4 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(deleteUserCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// deleteUserCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// deleteUserCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
